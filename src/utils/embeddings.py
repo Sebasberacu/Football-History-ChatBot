a59508b1324a6540d30d1c32a2607a411baebea1
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2024-06-08 12:20:05 -0600

    Add embeddings file

---
+++ src/utils/embeddings.py
@@ -0,0 +1,24 @@
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+
+def split_text(pdf_text):
+    document = Document(page_content=pdf_text)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    documents = text_splitter.split_documents([document])
+    return documents
+
+def generate_embeddings(documents, index_path="faiss_index"):
+    embeddings = OllamaEmbeddings(model='mxbai-embed-large')
+    db = FAISS.from_documents(documents, embeddings)
+    db.save_local(index_path)
+
+def check_faiss_vectorstore(db):
+    try:
+        index_info = db.index
+        print("FAISS vector store contains:", index_info.ntotal, "documents")
+        return True
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return False 
